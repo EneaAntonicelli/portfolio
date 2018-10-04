849ba7ddde93154b3ae1d0f6b33e51120ea1bfd7
Enea <eneasecondary@gmail.com>
2018-10-04 09:58:39 -0700

    Installed express. Added server routes. Modified html to route correctly.

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+const path = require("path"); 
 
 
+app.use(express.static("public"));
+app.use(express.static("views"));
 
 router.get("/", (req,res) => {
-    res.json("Home page route");
-})
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
 
 router.get("/resume", (req,res) => {
     res.json("Resume page route");
-})
+});
 
 router.get("*", (req,res) => {
     res.json("404 error page route");
-})
+});
 
 app.use("/", router);
 
 
 app.listen(8080, () => {
     console.log("App is listening on port ", 8080);
-})
+});
