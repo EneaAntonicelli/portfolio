48f4885229b9ed47873e967bc75297ce3c871621
Enea <eneasecondary@gmail.com>
2018-10-04 12:00:54 -0700

    Added a post route for the contact form

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const path = require("path"); 
+const bodyParser = require("body-parser");
 
 
 app.use(express.static("public"));
 app.use(express.static("views"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// GET ROUTES:
 
 router.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "./index.html"));
@@ -18,6 +23,29 @@ router.get("/resume", (req,res) => {
 router.get("*", (req,res) => {
     res.json("404 error page route");
 });
+
+// POST ROUTES:
+
+router.post("/user", (req,res) => {
+
+    console.log ("req is " + req.body);
+
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let country = req.body.country;
+    let subject = req.body.subject;
+
+    let newUser = {
+        firstname:firstname,
+        lastname:lastname,
+        country:country,
+        subject:subject
+    }
+
+    res.json({msg: "Your data ", data: JSON.stringify(newUser) + "has been sent."})
+
+})
+
 
 app.use("/", router);
 
