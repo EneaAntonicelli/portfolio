abd74c5a4d1bfd516362acf2475f1643e4293a7a
Enea <eneasecondary@gmail.com>
2018-10-04 08:55:37 -0700

    Added routes

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require("express");
+const app = express();
+const router = express.Router();
+
+
+
+router.get("/", (req,res) => {
+    res.json("Home page route");
+})
+
+router.get("/resume", (req,res) => {
+    res.json("Resume page route");
+})
+
+router.get("*", (req,res) => {
+    res.json("404 error page route");
+})
+
+app.use("/", router);
+
+
+app.listen(8080, () => {
+    console.log("App is listening on port ", 8080);
+})
