7366be75fb65462d286e030a684f17bd3a1f2d8b
Enea <eneasecondary@gmail.com>
2018-10-07 00:39:13 -0700

    final commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 require('dotenv').config()
 const router = require("./routes/routing");
 
+var PORT = process.env.PORT || 8080;
+
 app.use(express.static("public"));
 app.use(express.static("views"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,8 +14,8 @@ app.use(bodyParser.text());
 
 app.use("/", router);
 
-app.listen(8080, () => {
-    console.log("App is listening on port ", 8080);
+app.listen(PORT, () => {
+    console.log("App is listening on port ", PORT);
 });
 
 
